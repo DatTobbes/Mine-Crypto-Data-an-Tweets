7e7bbaf430c5a20259e869de6bdd7b2386d41d6d
markusgl <markus.glas@gmx.net>
2018-04-13 11:40:46 +0200

    add logging + refactoring

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -3,31 +3,39 @@ from tweepy import OAuthHandler, Stream
 from tweepy.streaming import StreamListener
 import time
 import json
-from SentimentAnalyze import SentimentAnalyzer
+from Twitter.SentimentAnalyze import SentimentAnalyzer
 from Database.db_mySql import MySqlDbConnector
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from Service.AskCoincap import CoinIoReader
 from sqlalchemy import create_engine
+import logging
 
+logging.basicConfig(filename='twittermining.log', filemode='a', level=logging.INFO,
+                            format='%(asctime)s %(message)s')
+logger = logging.getLogger(__name__)
+DB_USER = 'root'
+DB_PASSWORD = ''
+DB_NAME = 'twitterneu'
+DB_CONNECTION_STRING = 'mysql+pymysql://{}:{}@localhost:3306/{}?charset=utf8mb4'.format(DB_USER, DB_PASSWORD, DB_NAME)
 
 class CryptoListner(StreamListener):
     def __init__(self):
         self.analyzer = SentimentAnalyzer()
-        self.db_connector= MySqlDbConnector('localhost', 3306, 'root', '', 'coindata')
-        self.db_connector.create_tweets_tabel()
-        self.engine = create_engine('mysql+pymysql://root:@localhost:3306/coindata?charset=utf8mb4', echo=False)
+        #self.db_connector= MySqlDbConnector('localhost', 3306, 'root', '', 'coindata')
+        #self.db_connector.create_tweets_tabel()
+        self.engine = create_engine(DB_CONNECTION_STRING, echo=False)
         #self.engine = create_engine('mysql+mysqldb://root:@localhost:3306/coindata?charset=utf8', echo=False, convert_unicode=True, encoding= 'utf8')
         self.coin_cap_reader=CoinIoReader()
         self.tweet_array= np.empty((1,9))
 
     def __wait_till_start(self):
         from datetime import datetime
-        while datetime.now().minute % 15 != 0:
+        while datetime.now().minute % 1 != 0:  # TODO change 1 to 15 for prod
             time.sleep(1)
-            print(datetime.now())
-        print('Starting at' + str(datetime.now()))
+            #print(datetime.now())
+        logger.info('Starting at' + str(datetime.now()))
 
     def __get_actual_btc_price(self):
         btc_price=json.loads(self.coin_cap_reader.getCoinCapData('page/BTC').text)
@@ -39,7 +47,8 @@ class CryptoListner(StreamListener):
         act_time = time.time()
 
         time_to_sleep = time_to_wait - (act_time - start_time)
-        time.sleep(time_to_sleep)
+        logger.info('Sleeping ' + str("%.2f" % (time_to_sleep / 60)) + ' min')
+        time.sleep(abs(time_to_sleep))
 
         end_price = self.__get_actual_btc_price()
 
@@ -119,11 +128,11 @@ class CryptoListner(StreamListener):
             self.tweet_array= np.vstack([self.tweet_array, tweet])
             return True
         except BaseException as e:
-            print("Error on_data: %s" % str(e))
+            logger.error("Error on_data: %s" % str(e))
         return True
 
     def on_error(self, status):
-        print(status)
+        logger.error("Connection error: %s" % status)
         return True
 
     def stream_tweets(self):
@@ -140,7 +149,7 @@ class CryptoListner(StreamListener):
             #Kann zum debuggen einkommentiert werden
             #diff, start, end=1,2,3
             #time.sleep(10)
-            diff, start, end= self.__get_btc_and_wait(900)
+            diff, start, end= self.__get_btc_and_wait(90) # TODO change to 900 for prod
             print("startprice: %.2f endprice: %.2f" %(start,end))
             df=self.__array_in_dataframe(diff, start, end)
             self.write_to_db(df)
